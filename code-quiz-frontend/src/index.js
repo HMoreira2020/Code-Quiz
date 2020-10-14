bf96a1fd9b277998e89a2ea22ea3e743b68b639d
HMoreira2020 <moreiraheather@gmail.com>
2020-10-13 20:06:26 -0700

    Add sign up form and function

--- code-quiz-frontend/src/index.js
+++ code-quiz-frontend/src/index.js
@@ -1,6 +1,38 @@
 console.log("testing...")
+const signInForm = document.getElementsById('myForm')[0]
 const startButton = document.getElementById('start-btn')
-startButton.addEventListener('click', startGame)
+// startButton.addEventListener('click', startGame)
+
+myForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const usersUrl = "http://localhost:3000/users"
+  let formData = {
+    name: document.getElementById("user-name").value;
+    email: document.getElementById("user-email").value;
+  };
+
+  fetch(usersUrl, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+      body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(function(json) {
+      console.log(json);
+      signInForm.classList.add("hide");
+    })
+    .catch(error => console.log(error));
+  //how do I sign in??? where do i grab the username input? I sent this users#create to create a user 
+  //then hide the sign in form. 
+  //does it pass through users#create? we are posting to users which is index? 
+  //when I submit it creates a new user and takes me to /users but only displays that user created....why does it redirect and why only that user if we are on /users? 
+  
+})
+
+
 
 function startGame() {
   console.log('Started')
