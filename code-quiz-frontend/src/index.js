868fc4ad35852fb4585a882a13960cc771c8c998
HMoreira2020 <moreiraheather@gmail.com>
2020-10-14 11:59:26 -0700

    User signs in and user is created, hide user sign in and display start button

--- code-quiz-frontend/src/index.js
+++ code-quiz-frontend/src/index.js
@@ -1,14 +1,15 @@
 console.log("testing...")
-const signInForm = document.getElementsById('myForm')[0]
+const signInForm = document.getElementById('myForm')[0]
 const startButton = document.getElementById('start-btn')
 // startButton.addEventListener('click', startGame)
+const form = document.getElementById('signin')
 
 myForm.addEventListener('submit', function(e) {
   e.preventDefault();
   const usersUrl = "http://localhost:3000/users"
   let formData = {
-    name: document.getElementById("user-name").value;
-    email: document.getElementById("user-email").value;
+    name: document.getElementById("user-name").value,
+    email: document.getElementById("user-email").value
   };
 
   fetch(usersUrl, {
@@ -22,11 +23,18 @@ myForm.addEventListener('submit', function(e) {
     .then(res => res.json())
     .then(function(json) {
       console.log(json);
-      signInForm.classList.add("hide");
+    //  let userEmail = json.email
+    // "skfj@gmail.com"
+    // let userName= json.name
+      // let userId = json.id 
+    // "Chris "  append these to a div to display user name 
+      form.classList.add("hide");
+      startButton.classList.remove("hide")
+
     })
     .catch(error => console.log(error));
   //how do I sign in??? where do i grab the username input? I sent this users#create to create a user 
-  //then hide the sign in form. 
+  //then hide the sign in form. How do I do that? it's taking me to a new page 
   //does it pass through users#create? we are posting to users which is index? 
   //when I submit it creates a new user and takes me to /users but only displays that user created....why does it redirect and why only that user if we are on /users? 
   
