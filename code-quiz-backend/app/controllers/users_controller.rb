bd639101bc7fa5cddf3d17eb68b643dbb8d66fa0
HMoreira2020 <moreiraheather@gmail.com>
2020-10-14 12:11:29 -0700

    Add associations between questions and quizzes

--- code-quiz-backend/app/controllers/users_controller.rb
+++ code-quiz-backend/app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
     def create
         user = User.create(name: params[:user][:name], email: params[:user][:email])
-        session[:id] = user.id
         render json: user
     end 
 end
---
+++ code-quiz-backend/db/migrate/20201014190714_add_ref_to_questions.rb
@@ -0,0 +1,5 @@
+class AddRefToQuestions < ActiveRecord::Migration[6.0]
+  def change
+      add_column :questions, :quiz_id, :integer 
+  end
+end
