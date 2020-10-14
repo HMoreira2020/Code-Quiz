0a714a67724e2432d4b990a07c39f1bbda485c87
HMoreira2020 <moreiraheather@gmail.com>
2020-10-13 20:03:08 -0700

    Create user sign up/create user

--- code-quiz-backend/app/controllers/users_controller.rb
+++ code-quiz-backend/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+     def index 
+        users = User.all 
+        render json: users
+     end 
+
+    def create
+        user = User.create(name: params[:user][:name], email: params[:user][:email])
+        render json: user
+    end 
 end
